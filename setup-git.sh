9eebb7d89177a04de80b7faaa0b4db034d873c3c
Jocelyn <jossferatu@gmail.com>
2024-08-10 16:12:39 +0000

    outline git setup script

---
+++ setup-git.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ssh-keygen -t ed25519 -C "jossferatu@gmail.com"
+cat ~/.ssh/id_ed25519_github-jossferatu.pub
+ps -ax | grep ssh-agent
+eval "$(ssh-agent -s)"
+ps -ax | grep ssh-agent
+ssh-add /home/jossy/.ssh/id_ed25519_github-jossferatu
+git config --global user.name Jocelyn Manger
+git config --global user.email jossferatu@gmail.com
+git config --global core.editor "vim"
+git config --global push.autoSetupRemote true
